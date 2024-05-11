5cb663c4778c4cd99c4b6e8c171a72994955c31d
TASPlasma <derekjmather@gmail.com>
2024-05-10 23:29:25 -0700

    Added positional encoding

--- positional_encoding.py
+++ positional_encoding.py
@@ -1,24 +1,5 @@
 from config import Config
 
-seq_len = 7
-d_m = 4
-
-n = 5
-
-shape = (seq_len, d_m)
-pos = jnp.arange(0, seq_len)[:, np.newaxis] * jnp.ones(shape)
-
-dims = jnp.arange(0, d_m) * jnp.ones((seq_len, d_m))
-
-pe = (dims % 2 == 0)*(jnp.sin(pos/(n ** dims)))
-pe = pe + + (dims % 2 == 1)*(jnp.cos(pos/(n ** (dims - 1))))
-
-print('dims: \n', dims)
-
-print('pos: \n', pos)
-
-print('pe: \n', pe)
-
 class PositionalEncoding(nn.Module):
     """
     Uses np/jnp wizardry that I cooked up but barely understand
