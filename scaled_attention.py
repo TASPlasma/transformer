ac63ec93f8a059cc5676a9b3f8eec3c1c00bf1bb
Derek <jazzfusion10@gmail.com>
2024-05-12 09:16:08 -0700

    added scaled attention formula

--- scaled_attention.py
+++ scaled_attention.py
@@ -4,9 +4,15 @@ class ScaledDotProdAttention(nn.Module):
     config: Config
 
     @nn.compact
-    def __call__(self):
+    def __call__(self, q, k, v):
         """
-        
+        q: (seq_len, d_model)
+        k: (seq_len, d_model)
+        v: (seq_len, d_model)
         """
         cfg = self.config
-        pass
+
+        attn = nn.softmax((q * k.transpose()) / jnp.sqrt(cfg.model_size)) * v
+
+        return attn
+        
